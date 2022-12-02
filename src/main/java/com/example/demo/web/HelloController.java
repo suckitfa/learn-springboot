2130c2aa6b44256603b866259cb12932b85d03f0
Bob Tang <254202042@qq.com>
2022-12-02 23:12:25 +0800

    init

---
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "hello world!";
+    }
+}
